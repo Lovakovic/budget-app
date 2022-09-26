e5a9f192eda4950b1b06c0cb6d247e7fc13670e7
Lovakovic <lovkovdario@gmail.com>
2022-09-26 12:57:40 +0200

    Initial boilerplate code commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<p>This is where I'd put my app, if I had one.</p>);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node-modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devServer: {
+        static: path.join(__dirname, 'public'),
+        port: 8080,
+        open: true,
+        hot: true
+    },
+    devtool: 'eval-cheap-module-source-map'
+}
